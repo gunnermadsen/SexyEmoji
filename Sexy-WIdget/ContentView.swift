4577ee1df5ac261b50a77f07037af9a8eadffdf8
gunnermadsen <gunner.madsen@outlook.com>
2021-11-13 11:25:24 -0800

    Initial Commit

---
+++ Sexy-WIdget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sexy-WIdget
+//
+//  Created by gunner madsen on 11/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sexy-WIdget/Sexy_WIdgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sexy_WIdgetApp.swift
+//  Sexy-WIdget
+//
+//  Created by gunner madsen on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Sexy_WIdgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
